6419e0bbdedf581f15304b5a6e74c8fbf01011b3
Glitch (fcc-uurlshortener) <none>
2018-05-28 11:57:32 +0000

    💰🏰 Updated with Glitch

--- server.js
+++ server.js
@@ -2,15 +2,19 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 var mongo=require('mongodb').MongoClient
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
+
+var collName='urls'
+var urlPrefix='https://fcc-uurlshortener.glitch.me/'
+var dbName = 'fcc-uurlshortener'
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/new/*", function (request, response) {
@@ -19,13 +23,31 @@ app.get("/new/*", function (request, response) {
   var req = request.url
   var requested = req.substring(5)
   mongo.connect(url,{useNewUrlParser:true},function(err,db) {
-    if (err) {
-      response.send(JSON.stringify(err))
-  } else {
-    response.send(requested)
-  }
-                
-})
+    if (err) {response.send(JSON.stringify(err))} 
+    var shortNum = 1000
+    var dbo=db.db(dbName)
+    var coll = dbo.collection(collName)
+    // get max number
+    var urlDoc = coll.find().sort({urlNum:-1}).limit(1)
+    // create new url
+    if (isNaN(urlDoc.urlNum)===false) {
+      shortNum = urlDoc.urlNum+1
+    }
+    // return link to user
+    var newUrl = 'https://fcc-uurlshortener.glitch.me/'+shortNum
+    var newDocObj = {"urlNum":shortNum,"url":newUrl}
+    
+    var newDoc= JSON.stringify(newDocObj)
+    console.log(newDoc)
+    
+    var coll = dbo.collection(collName)
+    coll.insert(newDocObj,function(err,data) {
+    if (err) throw err
+      
+    })
+    db.close()
+    response.send('<html><head><title>Shortened URL</title></head><body><a href="'+newUrl+'" target="_blank">'+newUrl+'</a></body></html>')
+  })
 })
 
 app.get("/*",function (request,response) {
@@ -37,17 +59,27 @@ app.get("/*",function (request,response) {
   mongo.connect(url,{useNewUrlParser:true},function(err,db) {
     if (err) {
       response.send(JSON.stringify(err))
-  } else {
-if (result=="") {
-    response.send("Create a new shortened URL with: https://fcc-uurlshortener.glitch.me/new/LONG_URL_HERE")
-  } else {
-    response.redirect(result)
-  }  
-  }                
+  } 
+    // get url associated with link
+    var dbo=db.db(dbName)
+    var coll = dbo.collection(collName)
+    var urlNum = parseInt(requested)
+    coll.find({urlNum:urlNum}).toArray(
+        function(err,doc) {
+          if (err) throw err
+          result=doc.url
+          db.close()
+        })  
+    if (result==="") {
+        response.send("Create a new shortened URL with: https://fcc-uurlshortener.glitch.me/new/LONG_URL_HERE")
+      } else {
+        response.redirect(result)
+      }  
+  })
 })
-  
-  
-})
+
+              
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
