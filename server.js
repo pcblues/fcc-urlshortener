8615a19eaba7add55ac70ca8d31f1b8d119dd05d
Glitch (fcc-uurlshortener) <none>
2018-05-28 03:22:04 +0000

    🍟🙏 Updated with Glitch

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+var mongo=require('mongodb').MongoClient
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -12,17 +13,43 @@ var app = express();
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  var result = {}
-  //{"ipaddress":"49.255.32.94","language":"en-US","software":"Windows NT 10.0; Win64; x64"}
-  result.ipaddress=request.header('x-forwarded-for').split(',')[0] || request.connection.remoteAddress
-  result.language=request.headers['accept-language'].split(',')[0]
-  result.software=request.headers['user-agent'].split('(')[1].split(')')[0]
-  var resp = JSON.stringify(result)
-  response.send(resp);
-});
+app.get("/new/*", function (request, response) {
+  var url = process.env.MONGODB_URL
+  console.log(url)
+  var req = request.url
+  var requested = req.substring(5)
+  mongo.connect(url,{useNewUrlParser:true},function(err,db) {
+    if (err) {
+      response.send(JSON.stringify(err))
+  } else {
+    response.send(requested)
+  }
+                
+})
+})
+
+app.get("/*",function (request,response) {
+  var result=""
+  var url = process.env.MONGODB_URL  
+  var req = request.url
+  var requested = req.substring(5)
+  console.log(requested)
+  mongo.connect(url,{useNewUrlParser:true},function(err,db) {
+    if (err) {
+      response.send(JSON.stringify(err))
+  } else {
+if (result=="") {
+    response.send("Create a new shortened URL with: https://fcc-uurlshortener.glitch.me/new/LONG_URL_HERE")
+  } else {
+    response.redirect(result)
+  }  
+  }                
+})
+  
+  
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-});
+})
